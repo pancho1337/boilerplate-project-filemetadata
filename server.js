5c3241071e78d8ecee676151604054d358f552e6
root <root@DESKTOP-M86GBUV.localdomain>
2020-11-07 19:44:39 -0600

    simplefix

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 // require and use "multer"...
 
@@ -11,13 +13,36 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  console.log("upfile", req.file)
+  let resObj = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
+  res.json(resObj);
 });
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
 });
+
+// {
+//   "name": "images.jpg",
+//   "type": "image/jpeg",
+//   "size": 6756
+//   }
+
+// upfile {
+//   fieldname: 'upfile',
+//   originalname: 'images.jpg',
+//   encoding: '7bit',
+//   mimetype: 'image/jpeg',
+//   destination: 'uploads/',
+//   filename: '6c8230d6c48e91ef26577db9999feb6e',
+//   path: 'uploads/6c8230d6c48e91ef26577db9999feb6e',
+//   size: 6756
+// }
